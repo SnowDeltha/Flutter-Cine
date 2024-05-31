430bc2f6fdcfc49fb5d7746b3d04c3b9c68c5cd8
SnowDeltha <deicrolix@gmail.com>
2024-05-31 06:00:16 -0500

    environment variables configured

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 Future<void> main() async{
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
